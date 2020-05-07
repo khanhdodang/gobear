f96dafafdcb8ababa89ed4371e8f2e9b5f6037a3
Sa Kieu <kimsa.kieu@gmail.com>
2020-05-08 00:50:27 +0700

    Updates on code reviews

--- src/main/java/com/utils/FileUtils.java
+++ src/main/java/com/utils/FileUtils.java
@@ -1,43 +1,57 @@
 package com.utils;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 import java.io.*;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class FileUtils {
 
-    public List<List<String>> readCSV(String filePath) throws IOException {
-        InputStream is = FileUtils.class.getResourceAsStream(filePath);
+    public List<List<String>> readCSV(String filePath, String splitBy) {
+        InputStream is = getClass().getResourceAsStream(filePath);
         BufferedReader csvReader = new BufferedReader(new InputStreamReader(is));
         List<List<String>> records = new ArrayList<>();
         String row;
-        while ((row = csvReader.readLine()) != null) {
-            String[] data = row.split(",");
-            records.add(Arrays.asList(data));
+        try {
+            while ((row = csvReader.readLine()) != null) {
+                String[] data = row.trim().split(splitBy);
+                records.add(Arrays.asList(data));
+            }
+            csvReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        csvReader.close();
         return records;
     }
 
-    public JSONObject readJSON(String filePath) throws IOException {
-        InputStream is = FileUtils.class.getResourceAsStream(filePath);
+    public JSONObject readJSON(String filePath) {
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonObject = null;
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is));) {
+        try (InputStream is = getClass().getResourceAsStream(filePath);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
             jsonObject = (JSONObject) jsonParser.parse(reader);
             System.out.println(jsonObject);
-        } catch (ParseException e) {
+        } catch (ParseException | IOException e) {
             e.printStackTrace();
         }
         return jsonObject;
+    }
+
+    public void parseEmployeeData(JSONObject json){
+        JSONObject employees = (JSONObject) json.get("employees");
+        JSONArray employee = (JSONArray) employees.get("employee");
+        System.out.println("Employee...");
+        for (Object e : employee) {
+            JSONObject eJson = (JSONObject) e;
+            System.out.println("id: " + eJson.get("id"));
+            System.out.println("name: " + eJson.get("firstName") + " " + eJson.get("lastName"));
+        }
     }
 }
--- src/test/java/com/testcases/Verify_Download_Files.java
+++ src/test/java/com/testcases/Verify_Download_Files.java
@@ -13,30 +13,21 @@ public class Verify_Download_Files {
     FileUtils fileUtils = new FileUtils();
 
     @Test(description = "Read CSV file in local.")
-    public void TestCase4() throws IOException {
+    public void TestCase4(){
         System.out.println("------------Read CSV file in local------------");
-    String filePath = "/downloads/car.csv";
-        List<List<String>> records = fileUtils.readCSV(filePath);
-        for (List<String>record : records){
+        String filePath = "/downloads/car.csv";
+        String splitBy = ",";
+        List<List<String>> records = fileUtils.readCSV(filePath, splitBy);
+        for (List<String> record: records) {
             System.out.println(record);
         }
-
     }
 
     @Test(description = "Read JSON file in local.")
-    public void TestCase5() throws IOException {
+    public void TestCase5(){
         System.out.println("------------Read JSON file in local------------");
         String filePath = "/downloads/employees.json";
         JSONObject json = fileUtils.readJSON(filePath);
-        JSONObject employees = (JSONObject) json.get("employees");
-        JSONArray employee = (JSONArray) employees.get("employee");
-        for (Object e : employee)
-        {
-            JSONObject eJson = (JSONObject) e;
-            System.out.println(e +"");
-            System.out.println("id..." + eJson.get("id"));
-            System.out.println("name..." + eJson.get("firstName") + " " + eJson.get("lastName"));
-            
-        }
+        fileUtils.parseEmployeeData(json);
     }
 }
