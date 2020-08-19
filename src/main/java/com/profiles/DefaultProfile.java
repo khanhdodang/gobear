ccb9af6927aad486db5d3b6ca9400d5590e27a4f
Khanh Do <khanhdo.pmp@gmail.com>
2020-08-19 19:25:11 +0700

    Update DefaultProfile.java

--- src/main/java/com/profiles/DefaultProfile.java
+++ src/main/java/com/profiles/DefaultProfile.java
@@ -4,7 +4,7 @@ public class DefaultProfile {
 
     public static String urlUAT = "https://www.gobear.com/ph?x_session_type=UAT";
     public static String MAC_CHROME_DRIVER = "/drivers/chromedriver";
-    public static String MAC_FIREFOX_DRIVER = "/usr/local/Cellar/geckodriver/0.26.0/bin/geckodriver";
+    public static String MAC_FIREFOX_DRIVER = "/drivers/geckodriver";
     public static String PATH_CSV = "/downloads/car.csv";
     public static String DOWNLOAD_PATH = "/downloads";
     public static int WAIT_PRESENT_TIMEOUT = 30;
